fc56299104ec329f6c1ea63746093234ac12c015
Finnik <73883383+golkity@users.noreply.github.com>
2025-01-14 20:09:20 +0300

    Fix: typedata: string -> float64

--- internal/models/book.go
+++ internal/models/book.go
@@ -1,8 +1,8 @@
 package models
 
 type Book struct {
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-	Price  string `json:"price"`
+	ID     int     `json:"id"`
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Price  float64 `json:"price"`
 }
