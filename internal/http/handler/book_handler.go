112175dc48399b33d20f8de945a61c7e09868cc3
Finnik <73883383+golkity@users.noreply.github.com>
2025-01-14 19:39:05 +0300

    Update: new func(addbook && getbooks)

--- internal/http/handler/book_handler.go
+++ internal/http/handler/book_handler.go
@@ -1,7 +1,39 @@
 package handler
 
-import "RestApi/pkg/logger"
+import (
+	"RestApi/internal/models"
+	"RestApi/internal/storage"
+	"RestApi/pkg/logger"
+	"encoding/json"
+	"net/http"
+)
 
 type BookHandler struct {
-	log *logger.Logger
+	log     *logger.Logger
+	storage *storage.MemoryStorage
+}
+
+func NewBookHandler(log *logger.Logger, storage *storage.MemoryStorage) *BookHandler {
+	return &BookHandler{log: log, storage: storage}
+}
+
+func (h *BookHandler) GetBooks(w http.ResponseWriter, r *http.Request) {
+	h.log.Info("Fetching all books")
+	books := h.storage.GetBooks()
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func (h *BookHandler) AddBook(w http.ResponseWriter, r *http.Request) {
+	var book models.Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	h.storage.AddBooks(book)
+	h.log.Info("Book added successfully")
+
+	w.WriteHeader(http.StatusCreated)
 }
