8c25ab8768a07c8a7afcd0cc24c615c1032056b1
Finnik <73883383+golkity@users.noreply.github.com>
2025-01-14 19:42:21 +0300

    Update: new func(getbookid && updatebook && deletbook)

--- internal/http/handler/book_handler.go
+++ internal/http/handler/book_handler.go
@@ -6,6 +6,7 @@ import (
 	"RestApi/pkg/logger"
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 type BookHandler struct {
@@ -36,4 +37,57 @@ func (h *BookHandler) AddBook(w http.ResponseWriter, r *http.Request) {
 	h.log.Info("Book added successfully")
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *BookHandler) GetBookId(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest) //
+		return
+	}
+
+	book, err := h.storage.GetBook(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound) //404
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(book)
+}
+
+func (h *BookHandler) UpdateBook(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil {
+		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	var updatedBook models.Book
+	if err := json.NewDecoder(r.Body).Decode(&updatedBook); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := h.storage.UpdateBook(id, updatedBook); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	h.log.Info("Book updated successfully")
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *BookHandler) DeleteBook(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil {
+		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.storage.DeleteBook(id); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	h.log.Info("Book deleted successfully")
+	w.WriteHeader(http.StatusOK)
 }
