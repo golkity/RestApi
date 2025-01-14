147facbed0b0b162eab161926de643b69dc4b11f
Finnik <73883383+golkity@users.noreply.github.com>
2025-01-14 19:45:41 +0300

    Update: upload handlefunc(get,pos,put,delet

--- internal/http/server/server.go
+++ internal/http/server/server.go
@@ -1,10 +1,25 @@
 package server
 
 import (
-	"log"
+	"RestApi/internal/http/handler"
+	storage "RestApi/internal/storage"
+	"RestApi/pkg/logger"
 	"net/http"
 )
 
-func RegRoutes(mux *http.ServeMux, log *log.Logger) {
+func RegRoutes(mux *http.ServeMux, log *logger.Logger) {
+	storage := storage.NewMemoryStorage()
+	bookHandler := handler.NewBookHandler(log, storage)
 
+	mux.HandleFunc("/api/v1/books", bookHandler.GetBooks)
+	mux.HandleFunc("/api/v1/books/add", bookHandler.AddBook)
+	mux.HandleFunc("/api/v1/books/get", bookHandler.GetBookId)
+	mux.HandleFunc("/api/v1/books/update", bookHandler.UpdateBook)
+	mux.HandleFunc("/api/v1/books/delete", bookHandler.DeleteBook)
+
+	// Health check
+	mux.HandleFunc("/api/v1/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Health server - OK"))
+	})
 }
